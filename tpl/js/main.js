4d0e88f644d4f05dce5169cc23d37e27cefcc739
Scooter)Ok <scooterok@gmail.com>
2014-09-02 23:34:40 +0300

    promo

--- tpl/js/main.js
+++ tpl/js/main.js
@@ -1,1 +1,13 @@
-
+$(function(){
+    var dHeight = $(window).height();
+    $('.display-slide').height(dHeight);
+    $(window).on('resize', function(){
+        var dHeight = $(window).height();
+        $('.display-slide').height(dHeight);
+    });
+    $('.to-slide').on('click', function(e){
+        var toSlide = $(e.currentTarget).attr('data-slide');
+        var point = $('#slide'+toSlide).offset().top;
+        $('html, body').animate({scrollTop : point},900, 'easeInOutQuint');
+    });
+})
