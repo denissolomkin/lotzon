b92b27acd919ecf337987c00ccc735e3368e4d62
Scooter)Ok <scooterok@gmail.com>
2014-09-13 17:11:42 +0300

    news

--- tpl/js/main.js
+++ tpl/js/main.js
@@ -1,82 +1,4 @@
 $(function(){
-    var dHeight = $(window).height();
-    $('.display-slide').height(dHeight);
-    $(window).on('resize', function(){
-        var dHeight = $(window).height();
-        $('.display-slide').height(dHeight);
-    });
-    $('.to-slide').on('click', function(e){
-        var toSlide = $(e.currentTarget).attr('data-slide');
-        var point = $('#slide'+toSlide).offset().top;
-        $('html, body').animate({scrollTop : point},900, 'easeInOutQuint');
-    });
-    $('.go-play').on('click', function(){
-        $('#login-block').css('display','block');
-        setTimeout(function(){
-            $('#login-block').css('opacity','1');
-        },1)
-    });
-    $('#lb-close').on('click', function(){
-        $('#login-block').css('opacity','0');
-        setTimeout(function(){
-            $('#login-block').css('display','none');
-        },500)
-    });
-    $('#login-block').click(function(event) {
-        if ($(event.target).closest("#cl-check").length){
-            if($(event.target).hasClass('tb_a-l') && $(event.target).closest('#cl-check').hasClass('registration')){
-                $('#cl-check').toggleClass('registration login')
-            }else if($(event.target).hasClass('tb_a-r') && $(event.target).closest('#cl-check').hasClass('login')){
-                $('#cl-check').toggleClass('login registration')
-            }else if($(event.target).hasClass('r-p')){
-                $('.login .m_input').val('');
-                $(this).removeClass('error');
-            }
-            return;
-        }else{
-            $('#login-block').css('opacity','0');
-            setTimeout(function(){
-                $('#login-block').css('display','none');
-            },500)
-
-        }
-    });
-
-
-
-    // Contact form functional //
-    $('#cf-ab').on('click', function(){
-        if(!$(this).hasClass('ct-on')){
-            $(this).addClass('ct-on');
-            $('.fb-p-b').css('left',5000);
-            setTimeout(function(){
-                $('.fb-p-b, .fb-f-b').addClass('ct-on');
-                setTimeout(function(){
-                    $('.fb-f-b').css('left',0);
-                }, 50);
-            }, 200);
-        }else{
-            $(this).removeClass('ct-on');
-            $('.fb-f-b').css('left',-5000);
-            setTimeout(function(){
-                $('.fb-p-b, .fb-f-b').removeClass('ct-on');
-                setTimeout(function(){
-                    $('.fb-p-b').css('left',0);
-                }, 50);
-            }, 200);
-        }
-
-
-    });
-
-    $('#cti').val('');
-    $('#cti').on('keyup', function(){
-        $(this).height($(this).get(0).scrollHeight);
-        var $this = $(this);
-        $this.height(1);
-        $this.height(this.scrollHeight);
-    });
-
 
     // Navigations scroll functional //
 
@@ -91,12 +13,16 @@ $(function(){
     var navPos = $('nav.top-nav').offset().top;
     var tikets = $('.tickets').offset().top;
     var prizes = $('.prizes').offset().top;
+    var news = $('.news').offset().top;
     if($(document).scrollTop() >= 0 && $(document).scrollTop() < (prizes - 300)){
         $('.tn-mbk_li').removeClass('now');
         $('#tickets-but').addClass('now');
-    }else if($(document).scrollTop() > (prizes - 300)){
+    }else if($(document).scrollTop() > (prizes - 300) && $(document).scrollTop() < (news - 300)){
         $('.tn-mbk_li').removeClass('now');
         $('#prizes-but').addClass('now');
+    }else if($(document).scrollTop() > (news - 300)){
+        $('.tn-mbk_li').removeClass('now');
+        $('#news-but').addClass('now');
     }
     if($(document).scrollTop() > navPos){
         $('nav.top-nav').addClass('fixed');
@@ -106,13 +32,18 @@ $(function(){
     $(document).on('scroll', function(){
         tikets = $('.tickets').offset().top;
         prizes = $('.prizes').offset().top;
+        news = $('.news').offset().top;
         if($(document).scrollTop() >= 0 && $(document).scrollTop() < (prizes - 300)){
             $('.tn-mbk_li').removeClass('now');
             $('#tickets-but').addClass('now');
-        }else if($(document).scrollTop() > (prizes - 300)){
+        }else if($(document).scrollTop() > (prizes - 300) && $(document).scrollTop() < (news - 300)){
             $('.tn-mbk_li').removeClass('now');
             $('#prizes-but').addClass('now');
-        }
+        }else if($(document).scrollTop() > (news - 300)){
+            $('.tn-mbk_li').removeClass('now');
+            $('#news-but').addClass('now');
+        };
+
 
         if($(document).scrollTop() > navPos){
             $('nav.top-nav').addClass('fixed');
