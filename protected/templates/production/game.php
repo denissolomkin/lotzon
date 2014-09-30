af4b3cfe669f2225f1bb9f3920ac0b0a4033ab30
root <root@localhost.localdomain>
2014-10-01 00:59:47 +0300

    return working functional after merge

--- protected/templates/production/game.php
+++ protected/templates/production/game.php
@@ -670,89 +670,34 @@
                                         <div class="fc-bk">
                                             <div class="fc-nbs-bk">
                                                 <ul>
-                                                    <li>1</li>
-                                                    <li>2</li>
-                                                    <li>3</li>
-                                                    <li>4</li>
-                                                    <li>5</li>
-                                                    <li>6</li>
-                                                    <li>7</li>
-                                                    <li>8</li>
-                                                    <li>9</li>
-                                                    <li>10</li>
-                                                    <li>11</li>
-                                                    <li>12</li>
-                                                    <li>13</li>
-                                                    <li>14</li>
-                                                    <li>15</li>
-                                                    <li>16</li>
-                                                    <li>17</li>
-                                                    <li>18</li>
-                                                    <li>19</li>
-                                                    <li>20</li>
-                                                    <li>21</li>
-                                                    <li>22</li>
-                                                    <li>23</li>
-                                                    <li>24</li>
-                                                    <li>25</li>
-                                                    <li>26</li>
-                                                    <li>27</li>
-                                                    <li>28</li>
-                                                    <li>29</li>
-                                                    <li>30</li>
-                                                    <li>31</li>
-                                                    <li>32</li>
-                                                    <li>33</li>
-                                                    <li>34</li>
-                                                    <li>35</li>
-                                                    <li>36</li>
-                                                    <li>37</li>
-                                                    <li>38</li>
-                                                    <li>39</li>
-                                                    <li>40</li>
-                                                    <li>41</li>
-                                                    <li>42</li>
-                                                    <li>43</li>
-                                                    <li>44</li>
-                                                    <li>45</li>
-                                                    <li>46</li>
-                                                    <li>47</li>
-                                                    <li>48</li>
-                                                    <li>49</li>
+                                                    <? for ($i = 1; $i <= 49; ++$i) { ?>
+                                                        <li<?=(in_array($i, $player->getFavoriteCombination()) ? ' class="dis"' : '')?>><?=$i?></li>
+                                                    <? } ?>
                                                 </ul>
                                             </div>
                                             <div class="fc-tl">любимая комбинация</div>
                                             <ul class="fc-nrch-bk">
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
-                                                <li>
-                                                    <i></i>
-                                                    <span></span>
-                                                </li>
+                                                <? if (count($player->getFavoriteCombination())) { ?>
+                                                    <? foreach ($player->getFavoriteCombination() as $num) { ?>
+                                                        <li>
+                                                            <i></i>
+                                                            <span><?=$num?></span>
+                                                        </li>    
+                                                    <? } ?>
+                                                <? } else { ?>
+                                                    <? for ($i = 1; $i <= 6; ++$i) { ?>
+                                                        <li>
+                                                            <i></i>
+                                                            <span></span>
+                                                        </li>    
+                                                    <? } ?>
+                                                <? } ?>
                                             </ul>
                                         </div>
                                     </div>
                                     <div class="save-bk">
                                         <div class="sb-ch-td">
-                                            <input type="checkbox" id="rulcheck" hidden />
+                                            <input type="checkbox" name="visible" id="rulcheck" hidden <?=($player->getVisibility() ? "checked" : "")?> />
                                             <label for="rulcheck">Показывать мое имя<br/>в списке победителей</label>
                                         </div>
                                         <div class="sb-ch-td">
@@ -781,6 +726,10 @@
         <script src="/tpl/js/backend.js"></script>
         <script src="/tpl/js/main.js"></script>
     <script>
+        var playerFavorite = [];
+        <? foreach ($player->getFavoriteCombination() as $num) { ?>
+        playerFavorite.push(<?=$num?>);
+        <? } ?>
         $(function(){
             var ios;
             var mac = navigator.userAgent.indexOf('Mac OS');
