4f8be40d190e4b31cbf6c39e657c66b0d01a6ab9
Alexander Petushkov <ravanger@kntele.com>
2014-11-01 17:59:15 +0000

    jjj

--- protected/templates/production/trailer.php
+++ protected/templates/production/trailer.php
@@ -24,31 +24,24 @@
         <meta property="og:image" content="http://lotzon.com/tpl/img/social-share.jpg" /-->
 
         <!-- Schema.org markup for Google+ -->
-<meta itemprop="name" content="The Name or Title Here">
-<meta itemprop="description" content="This is the page description">
-<meta itemprop="image" content="http://lotzon.com/tpl/img/social-share.jpg">
+        <meta itemprop="name" content="<?=$seo['Title']?>">
+        <meta itemprop="description" content="Здесь реально можно выиграть Jackpot!">
+        <meta itemprop="image" content="http://lotzon.com/tpl/img/social-share.jpg">
 
-<!-- Twitter Card data -->
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@publisher_handle">
-<meta name="twitter:title" content="Page Title">
-<meta name="twitter:description" content="Page description less than 200 characters">
-<meta name="twitter:creator" content="@author_handle">
-<!-- Twitter summary card with large image must be at least 280x150px -->
-<meta name="twitter:image:src" content="http://lotzon.com/tpl/img/social-share.jpg">
+        <!-- Twitter Card data -->
+        <meta name="twitter:card" content="http://lotzon.com/tpl/img/social-share.jpg">
+        <meta name="twitter:title" content="<?=$seo['Title']?>">
+        <meta name="twitter:description" content="Здесь реально можно выиграть Jackpot!">
+        <meta name="twitter:creator" content="@author_handle">
+        <!-- Twitter summary card with large image must be at least 280x150px -->
+        <meta name="twitter:image:src" content="http://lotzon.com/tpl/img/social-share.jpg">
 
-<!-- Open Graph data -->
-<meta property="og:title" content="Title Here" />
-<meta property="og:type" content="article" />
-<meta property="og:url" content="http://www.example.com/" />
-<meta property="og:image" content="http://lotzon.com/tpl/img/social-share.jpg" />
-<meta property="og:description" content="Description Here" />
-<meta property="og:site_name" content="Site Name, i.e. Moz" />
-<meta property="article:published_time" content="2013-09-17T05:59:00+01:00" />
-<meta property="article:modified_time" content="2013-09-16T19:08:47+01:00" />
-<meta property="article:section" content="Article Section" />
-<meta property="article:tag" content="Article Tag" />
-<meta property="fb:admins" content="Facebook numberic ID" />
+        <!-- Open Graph data -->
+        <meta property="og:title" content="<?=$seo['Title']?>" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="http://www.lotzon.com/" />
+        <meta property="og:image" content="http://lotzon.com/tpl/img/social-share.jpg" />
+        <meta property="og:description" content="Здесь реально можно выиграть Jackpot!" />
 
         <link rel="stylesheet" href="/tpl/css/normalize.css" />
         <link rel="stylesheet" href="/tpl/css/trailer.css" />
