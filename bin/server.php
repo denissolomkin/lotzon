f409ad80655afeb45c883b46bcac8ac275a66ecf
Denis Solomkin <solomkin@gmail.com>
2015-01-19 16:17:59 +0200

    update game

--- bin/server.php
+++ bin/server.php
@@ -4,7 +4,9 @@ use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\Session\SessionProvider;
 use Ratchet\WebSocket\WsServer;
+use React\Socket\Server;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\MemcacheSessionHandler;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/pathes.php';
 // load application class
@@ -14,20 +16,45 @@ require_once PATH_APPLICATION . 'Application.php';
 
 $memcache = new \Memcache;
 $memcache->connect('localhost', 11211);
+
+
+
+$loop = \React\EventLoop\Factory::create();
+$webSock = new Server($loop);
+$webSock->listen('8080', '0.0.0.0');
+
+$server = new IoServer(
+    new HttpServer(new WsServer(
+        new SessionProvider(
+            new WebSocketController($loop),
+            new MemcacheSessionHandler($memcache)
+        )
+        )
+    ), $webSock
+);
+
+
+$loop->run();
+// $server->run();
+
+/*
+*/
+/*
+
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new SessionProvider(
-                new WebSocketController('shutDown'),
+                new WebSocketController(),
                 new MemcacheSessionHandler($memcache)
             )
         )
-    )
-    ,8080
+    ),8080
 );
 $server->run();
 
+*/
 function shutDown() {
     echo '1';
-    $server->loop->stop();
 }
