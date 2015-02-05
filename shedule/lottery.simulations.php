4418a5fbff51bdb40dd0b872e8db75895ac597dc
the7oker <valery.grygorash@gmail.com>
2015-02-05 04:22:19 +0200

    lottery hold fix

--- shedule/lottery.simulations.php
+++ shedule/lottery.simulations.php
@@ -1,14 +1,71 @@
 <?php
 
-require_once('lottery.inc.php');
+if(!file_exists($tmp = 'lottery.fix.95.tmp'))
+{
+	file_put_contents($tmp, '');
+
+	require_once('lottery.inc.php');
+
+	$lid = 95;
+
+	$SQL = "UPDATE
+							LotteryTickets	lt
+				INNER JOIN	LotterySettings	ls
+				ON			IFNULL(B6, 0) + IFNULL(B16, 0) + IFNULL(B26, 0) + IFNULL(B31, 0) + IFNULL(B37, 0) + IFNULL(B43, 0) = ls.BallsCount
+				INNER JOIN	Players p
+				ON			lt.PlayerId = p.Id
+				AND			CASE p.Country WHEN 'BY' THEN 'BY' WHEN 'UA' THEN 'UA' ELSE 'RU' END = ls.CountryCode
+			SET
+    			p.Points = p.Points - IF(ls.Currency = 'POINT', ls.Prize, 0),
+    			p.Money = p.Money - IF(ls.Currency = 'POINT', 0, ls.Prize)
+
+			WHERE
+				lt.LotteryId = $lid
+				AND (  B6  IS NOT NULL
+					OR B16 IS NOT NULL
+					OR B26 IS NOT NULL
+					OR B31 IS NOT NULL
+					OR B37 IS NOT NULL
+					OR B43 IS NOT NULL)";
+	DB::Connect()->query($SQL);
+
+	$SQL = "DELETE FROM
+				Transactions
+			WHERE
+					`Date`			= 1423076403
+				AND	`Description`	= 'Выигрыш в розыгрыше'";
+	DB::Connect()->query($SQL);
+
+	PlayerTotal($lid);
+	Transactions($lid);
+}
 
 if($_SERVER['argv'][1] == 'dbg')
 {
 	require_once('lottery.inc.php');
 
+/*
+	$comb = Array
+	(
+		'combination' => Array
+		(
+			'0' => 'B6',
+			'1' => 'B16',
+			'2' => 'B26',
+			'3' => 'B31',
+			'4' => 'B37',
+			'5' => 'B43',
+		),
+		'id' => $lid,
+	);
+
+	$comb['fields'] = $comb['combination'];
+
+	ApplyLotteryCombination($comb);
+
 //	ConverDB();
 //	RestoreAllTickets();
-
+/*
 	$rounds     = 50;
 	$ballsRange = 40;
 
@@ -16,5 +73,5 @@ if($_SERVER['argv'][1] == 'dbg')
 	sleep(20);
 
 	LotterySimulation('simulation.html', 0, $ballsRange, $rounds);
-
+// */
 }
