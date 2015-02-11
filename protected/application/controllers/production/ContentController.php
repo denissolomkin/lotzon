da22fc327daa616259c9dbbcaa3f5a16cec8f830
Denis Solomkin <solomkin@gmail.com>
2015-02-11 15:10:48 +0200

    fix game prices

--- protected/application/controllers/production/ContentController.php
+++ protected/application/controllers/production/ContentController.php
@@ -68,7 +68,6 @@ class ContentController extends \AjaxController
 
     public function bannerAction($sector)
     {
-        $resp=array();
         if(is_array(Config::instance()->banners[$sector]))
             foreach(Config::instance()->banners[$sector] as $group) {
                 if (is_array($group)) {
@@ -101,6 +100,7 @@ setTimeout(function(){ $('#ticket_video').remove(); }, ({$banner['title']}+1)*10
                         setTimeout(function(){ $('#ticket_video').remove(); }, ({$banner['title']}+1)*1000);
                         activateTicket();";
                         }
+
                         $resp['block'].="</script>";
                         break;
                     }
