7cdc27b211ec822c1bd2c613ca1e21928398c007
Denis Solomkin <solomkin@gmail.com>
2015-04-20 18:14:18 +0300

    banner country

--- protected/templates/admin/banners.php
+++ protected/templates/admin/banners.php
@@ -90,7 +90,7 @@
                         <div class="col-md-2" style="display: flex;">
                         <select name="banners[<?=$sid?>][<?=$gid?>][<?=$bid?>][countries][]" size="1" multiple="multiple" class="form-control-banner input-sm" value="" placeholder="Страны" />
                         <? foreach ($supportedCountries as $country) {?>
-                            <option <?=(is_array($banner['countries']) && array_search($country,$banner['countries'])!==false?' selected ':'');?> value="<?=$$country?>"><?=$country?></option>
+                            <option <?=(is_array($banner['countries']) && array_search($country,$banner['countries'])!==false?' selected ':'');?> value="<?=$country?>"><?=$country?></option>
                         <? } ?>
                         </select>
                             <button type="button" class="btn btn-info btn-xs view-banner right"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>
