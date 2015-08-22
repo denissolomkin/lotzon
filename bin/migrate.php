b5d512ed2ebc9d13fc2bbbe51bd46d6482875122
Denis Solomkin <solomkin@gmail.com>
2015-08-22 18:28:49 +0300

    []

---
+++ bin/migrate.php
@@ -0,0 +1,70 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// load pathes
+require_once dirname(__FILE__) . '/../pathes.php';
+
+// load application class
+require_once PATH_APPLICATION . 'Application.php';
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
+// load configs
+Application::import(PATH_SYSTEM . 'Config.php');
+Application::import(PATH_SYSTEM . '*');
+Application::import(PATH_CONFIGS . '*');
+Application::import(PATH_CONTROLLERS . '*');
+
+function getStoredMigrations()
+{
+    $migrations = array();
+    if (DB::Connect()->query("SHOW TABLES LIKE 'DatabaseMigrations'")->rowCount() > 0) {
+
+        $sql = "SELECT * FROM `DatabaseMigrations`";
+
+        try {
+            $sth = DB::Connect()->prepare($sql);
+            $sth->execute();
+        } catch (PDOException $e) {
+            throw new ModelException("Error processing storage query", 500);
+        }
+
+        $migrationsData = $sth->fetchAll();
+
+        foreach ($migrationsData as $data) {
+            $migrations[$data['Id']] = $data['File'];
+        }
+
+    }
+    return $migrations;
+}
+
+function commitMigrations($migrations = array())
+{
+    $path = dirname(__FILE__).'/../migrations';
+    if (is_dir($path) && ($openDir = opendir($path))) {
+        while (($file = readdir($openDir)) !== false) {
+            if ($file != "." && $file != "..") {
+                if (is_file($path.'/'.$file) && !in_array($file, $migrations)) {
+                    try {
+                        $sql = file_get_contents ($path.'/'.$file);
+
+                        DB::Connect()->prepare($sql)->execute();
+                        DB::Connect()->prepare("INSERT INTO `DatabaseMigrations` (`File`) VALUES (:f)")->execute(array(
+                            ':f'=>$file
+                        ));
+
+                        echo"\t$file [COMMIT]\n";
+                    } catch (PDOException $e) {
+                        throw new ModelException("Error processing storage query: ".$e->getMessage(), 500);
+                    }
+                }
+            }
+        }
+    }
+}
+
+$storedMigrations = getStoredMigrations();
+commitMigrations($storedMigrations);
---
+++ migrations/20150815_Create_Table_LotteryTicketsArchive.php
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `LotteryTicketsArchive` (
+`Id` int(11) NOT NULL AUTO_INCREMENT, 
+`LotteryId` int(11) NOT NULL DEFAULT '0', 
+`PlayerId` int(11) NOT NULL DEFAULT '0', 
+`Combination` varchar(255) NOT NULL,
+`DateCreated` int(11) NOT NULL DEFAULT '0',
+`TicketNum` tinyint(1) NOT NULL DEFAULT '1',
+`TicketWin` float(9,2) NOT NULL DEFAULT '0.00',
+`TicketWinCurrency` varchar(255) NOT NULL DEFAULT '',
+ PRIMARY KEY (`Id`),
+  KEY `idx_LotteryId` (`LotteryId`),
+  KEY `idx_PlayerId` (`PlayerId`)) 
+ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1
