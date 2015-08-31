9435df567638e00934bdcb4abf720b41267b31ac
Denis Solomkin <solomkin@gmail.com>
2015-08-31 14:29:10 +0300

    Update Migrations

--- migrations/20150829_Add_Columns_LastTicketId_In_Lotteries_And_IsGold_In_LotteryTicket_LotteryTicketsArchive.php
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `Lotteries` ADD `LastTicketId` BIGINT UNSIGNED NULL DEFAULT NULL AFTER `Combination`;
-ALTER TABLE `LotteryTickets` ADD `IsGold` BOOLEAN NOT NULL DEFAULT FALSE AFTER `TicketWinCurrency`;
