bdaf585e8780b2a29a85462dce37e4196c84f5d2
Denis Solomkin <solomkin@gmail.com>
2015-09-02 17:43:48 +0300

    Safe Rebases of Tables

---
+++ migrations/20150820_Rebase_Table_Transactions.php
@@ -0,0 +1,49 @@
+call isCol('Transactions','ObjectType','SELECT null',"
+CREATE TABLE IF NOT EXISTS `TransactionsTmp` (
+`Id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ObjectType` varchar(32) DEFAULT NULL,
+  `ObjectId` varchar(16) DEFAULT NULL,
+  `PlayerId` int(11) NOT NULL DEFAULT '0',
+  `Currency` varchar(20) NOT NULL DEFAULT '',
+  `Sum` float(9,2) NOT NULL DEFAULT '0.00',
+  `Balance` float(8,2) DEFAULT NULL,
+  `Description` varchar(255) NOT NULL DEFAULT '',
+  `Date` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+ALTER TABLE `TransactionsTmp` 
+ ADD KEY `idx_Admin` (`Date`,`ObjectType`),  ADD KEY `idx_Player` (`PlayerId`,`Date`,`ObjectType`), ADD KEY `idx_Object` (`ObjectType`,`ObjectId`)
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+INSERT INTO `TransactionsTmp` (`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`) 
+SELECT
+`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`
+FROM `Transactions` WHERE 1
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+INSERT INTO `TransactionsTmp` (`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`) 
+SELECT
+`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`
+FROM `Transactions` WHERE `Transactions`.Id > (SELECT max(Id) FROM `TransactionsTmp`)
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+INSERT INTO `TransactionsTmp` (`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`) 
+SELECT
+`Id`, `PlayerId`, `Currency`, `Sum`, `Balance`, `Description`, `Date`
+FROM `Transactions` WHERE `Transactions`.Id > (SELECT max(Id) FROM `TransactionsTmp`)
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+RENAME TABLE `Transactions` TO `TransactionsOld`
+");
+
+call isCol('Transactions','ObjectType','SELECT null',"
+RENAME TABLE `TransactionsTmp` TO `Transactions`
+");
+
+call dropTbl('TransactionsOld');
--- migrations/20150825_Add_Columns_Table_Transactions.php
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `Transactions` ADD  `ObjectType` VARCHAR( 32 ) NULL DEFAULT NULL AFTER  `Id` ,
-ADD  `ObjectId` VARCHAR( 16 ) NULL DEFAULT NULL AFTER  `ObjectType` ,
-ADD INDEX (  `ObjectType` ,  `ObjectId` )
