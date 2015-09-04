60260b0b34d4f5883b04174f84161a298f700602
Denis Solomkin <solomkin@gmail.com>
2015-09-04 11:58:09 +0300

    Add IsFee in PlayerGames

--- migrations/20150830_Rebase_Table_PlayerGames.php
+++ migrations/20150830_Rebase_Table_PlayerGames.php
@@ -12,8 +12,9 @@ CREATE TABLE IF NOT EXISTS `PlayerGamesTmp` (
   `Prize` float(9,2) DEFAULT NULL,
   `Result` tinyint(1) NOT NULL DEFAULT '0',
   `Currency` enum('MONEY','POINT','LOTZON') CHARACTER SET latin1 NOT NULL,
+  `IsFee` tinyint(1) DEFAULT NULL,
   `Price` float(9,2) NOT NULL,
-   KEY `MonthRating` (`GameId`,`Month`,`Price`),
+   KEY `Rating` (`Month`,`IsFee`),
    KEY `PlayerId` (`PlayerId`), 
    KEY `GameUid` (`GameUid`), 
    KEY `GameId` (`GameId`), 
@@ -22,23 +23,23 @@ CREATE TABLE IF NOT EXISTS `PlayerGamesTmp` (
 ");
 
 call isCol('PlayerGames','Month','SELECT null',"
-INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`) 
+INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`,`IsFee`, `Price`) 
 SELECT
-`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`
+`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, IF(`Price`=0,0,1), `Price`
 FROM `PlayerGames` WHERE `PlayerGames`.Id > IFNULL((SELECT max(Id) FROM `PlayerGamesTmp`),0)
 ");
 
 call isCol('PlayerGames','Month','SELECT null',"
-INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`) 
+INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `IsFee`, `Price`) 
 SELECT
-`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`
+`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, IF(`Price`=0,0,1), `Price`
 FROM `PlayerGames` WHERE `PlayerGames`.Id > (SELECT max(Id) FROM `PlayerGamesTmp`)
 ");
 
 call isCol('PlayerGames','Month','SELECT null',"
-INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`) 
+INSERT INTO `PlayerGamesTmp` (`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, `Month`, `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `IsFee`, `Price`) 
 SELECT
-`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, `Price`
+`Id`, `PlayerId`, `GameId`, `GameUid`, `Date`, UNIX_TIMESTAMP(DATE_FORMAT(FROM_UNIXTIME(`Date`),'%Y-%m-01')), `Win`, `Lose`, `Draw`, `Prize`, `Result`, `Currency`, IF(`Price`=0,0,1), `Price`
 FROM `PlayerGames` WHERE `PlayerGames`.Id > (SELECT max(Id) FROM `PlayerGamesTmp`)
 ");
 
