8265e45ad20698dc0d68f6476985c9aba19d677a
Denis Solomkin <solomkin@gmail.com>
2015-09-16 18:16:39 +0300

    Preparing for new design

---
+++ protected/templates/production/pages/prize_exchange_not_filled.php
@@ -0,0 +1,40 @@
+<div class="content-top">
+	<div class="content-main">
+
+		<div class="content-box exchange-prize">
+
+            <a href="javascript:history.back()"><div class="content-box-header back">Обмен приза</div></a>
+
+			<div class="content-box-content clearfix">
+				<div class="content-box-item">
+					<div class="ex-prize clearfix">
+						<div class="ex-prize-img">
+							<img src="/img/prize.png" alt="">
+						</div>
+						<div class="ex-prize-description">
+							<div class="ex-prize-name">Ноутбук Asus X551MA</div>
+							<div class="ex-prize-price">520 000<span>баллов</span></div>
+							<div class="ex-prize-details">
+								<p>
+									Баллы за приз будут автоматически списаны с Вашего счета.
+								</p>
+								<p>
+									В течении 7-ми рабочих дней с Вами свяжутся для уточнения информации.
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<!-- Данные НЕ заполнены -->
+					<div class="ex-prize-not-filled">
+						Информация для доставки приза отсутствует!<br>
+						Заполните ее в разделе <a href="cabinet_account_edit">«Мои данные»</a>
+					</div>
+					<!-- end of Данные НЕ заполнены -->
+				</div>
+			</div>
+
+		</div>
+
+	</div><!-- .content-main -->
+</div><!-- .content-top -->
---
+++ protected/templates/production/pages/tickets.php
@@ -0,0 +1,298 @@
+<div class="content-top">
+	<div class="content-main">
+
+		<div class="ticket-items">
+			<ul class="ticket-tabs clearfix">
+				<li class="active"><span>Билет </span>#1</li>
+				<li><span>Билет </span>#2</li>
+				<li><span>Билет </span>#3</li>
+				<li><span>Билет </span>#4</li>
+				<li><span>Билет </span>#5</li>
+				<li><span>Билет </span>#6</li>
+				<li><span>Билет </span>#7</li>
+				<li class="gold"><span>Билет </span>#8</li>
+			</ul><!-- .ticket-tabs -->
+
+			<div class="ticket-item clearfix">
+				<ul class="ticket-numbers clearfix">
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+					<li>4</li>
+					<li>5</li>
+					<li>6</li>
+					<li>7</li>
+					<li>8</li>
+					<li>9</li>
+					<li>10</li>
+					<li>11</li>
+					<li>12</li>
+					<li>13</li>
+					<li>14</li>
+					<li>15</li>
+					<li>16</li>
+					<li>17</li>
+					<li>18</li>
+					<li>19</li>
+					<li>20</li>
+					<li>21</li>
+					<li>22</li>
+					<li>23</li>
+					<li>24</li>
+					<li>25</li>
+					<li>26</li>
+					<li>27</li>
+					<li>28</li>
+					<li>29</li>
+					<li>30</li>
+					<li>31</li>
+					<li>32</li>
+					<li>33</li>
+					<li>34</li>
+					<li>35</li>
+					<li>36</li>
+					<li>37</li>
+					<li>38</li>
+					<li>39</li>
+					<li>40</li>
+					<li>41</li>
+					<li>42</li>
+					<li>43</li>
+					<li>44</li>
+					<li>45</li>
+					<li>46</li>
+					<li>47</li>
+					<li>48</li>
+					<li>49</li>
+				</ul><!-- .ticket-numbers -->
+
+				<ul class="ticket-actions">
+					<li>A</li>
+					<li class="ticket-fc-btn"></li>
+				</ul>
+
+				<div class="ticket-btn">Подтвердить</div>
+			</div><!-- .ticket-item -->
+		</div><!-- .ticket-items -->
+
+
+
+
+
+
+
+
+		<div class="matches-inf-wrapper carousel clearfix">
+			<div class="content-box matches-inf">
+				<div class="content-box-header">1-7 Билет</div>
+				<div class="content-box-content clearfix">
+						<table>
+							<tr>
+								<th>Совпадений в<br>одном билете</th>
+								<th>Сумма<br>выигрыша</th>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+								</td>
+								<td>100 000 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+								</td>
+								<td>200 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>10 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>25 коп</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>10 коп</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>5 баллов</td>
+							</tr>
+						</table>
+				</div>
+			</div>
+
+			<div class="content-box matches-inf gold">
+				<div class="content-box-header">8 Билет</div>
+				<div class="content-box-content clearfix">
+						<table>
+							<tr>
+								<th>Совпадений в<br>одном билете</th>
+								<th>Сумма<br>выигрыша</th>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+								</td>
+								<td>1 000 000 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+								</td>
+								<td>2 000 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>100 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>25 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>2 грн</td>
+							</tr>
+							<tr>
+								<td class="matches-balls">
+									<span class="true"></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</td>
+								<td>50 баллов</td>
+							</tr>
+						</table>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+		<div class="content-box tickets">
+
+			<div class="content-box-header">
+				<div class="content-box-tabs clearfix">
+					<a href="#" class="content-box-tab active">Правила</a>
+					<a href="#" class="content-box-tab">Частые вопросы</a>
+				</div>
+			</div>
+
+			<div class="content-box-content clearfix">
+				<div class="content-box-item tickets-rules">
+
+					<dl>
+						<dt>Как принять участие в розыгрыше?</dt>
+						<dd>
+							Принять участие в игре очень просто! Заполните игровой билет, выбрав 6 номеров из 49. Таких билетов предоставляется пять, что увеличивает шансы на выигрыш!
+						</dd>
+
+						<dt>Как проводится розыгрыш?</dt>
+						<dd>
+							Розыгрыш проводится каждый день в режиме онлайн по истечению времени на таймере.
+						</dd>
+
+						<dt>Как играть?</dt>
+						<dd>
+							Во время розыгрыша выпадает 6 призовых шаров. Вы выиграли, если угадали один и более призовых номеров в любом порядке.
+						</dd>
+
+						<dt>Это бесплатно?</dt>
+						<dd>
+							Во время розыгрыша выпадает 6 призовых шаров. Вы выиграли, если угадали один и более призовых номеров в любом порядке.
+						</dd>
+
+						<dt>Как принять участие в розыгрыше?</dt>
+						<dd>
+							Принять участие в игре очень просто! Заполните игровой билет, выбрав 6 номеров из 49. Таких билетов предоставляется пять, что увеличивает шансы на выигрыш!
+						</dd>
+					</dl>
+
+				</div>
+			</div>
+
+		</div>
+
+	</div><!-- .content-main -->
+</div><!-- .content-top -->
