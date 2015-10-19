61b04899ca270f8e0bb3de5d51daf72d08efe173
subsan <subsan@online.ua>
2015-10-19 18:36:16 +0300

    add email templates

---
+++ protected/templates/emails/blank.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>%header%</title>
+</head>
+
+<body style="margin:0 0 0 0;background-color:#f7f7f7">
+  <table style="background-color:#f7f7f7;width:100%;height:100%">
+    <tr>
+      <td valign="top">
+        <table style="width:90%; max-width:600px;margin:auto;">
+          <tr>
+            <td>
+              <div style="max-width:600px; border: 1px solid #d4d4d4">
+                <table width="100%" align="center" bgcolor="#ffffff" border="0" cellpadding="1" data-mobile="true">
+                  <tr>
+                    <td bgcolor="#ffe401" height="50px" width="100%">
+                      <table style="width:130px;height:100%">
+                        <tr style="width:130px;height:100%">
+                          <td background="http://nettel.com.ua/lotzone/logo.png" style="width:130px;min-width:28px;width:130px;height:100%">
+
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="30px" width="100%"></td>
+                  </tr>
+                  <tr>
+                    <td style="font-size: 16px; font-family: Arial;font-size:14px; padding-left:20px; padding-right:20px; line-height:20px">
+                      %body_text%
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="30px" width="100%"></td>
+                  </tr>
+                  <tr>
+                    <td style="background-color:#fafafa">
+                      <table style="width:100%;height:50px;background-color:#fafafa">
+                        <tr>
+                          <td style="width:20px"></td>
+                          <td style="font-family:Arial;font-size:13px">%footer_social%</td>
+                          <td style="width:28px;min-width:28px">
+                            <a href="***" style="width:100%;display:block;height:100%"><img style="width:28px;height:50px" src="http://nettel.com.ua/lotzone/fb.png"></a>
+                          </td>
+                          <td style="width:28px;min-width:28px">
+                            <a href="***" style="width:100%; display:block; height:100%"><img style="width:28px;height:50px" src="http://nettel.com.ua/lotzone/vk.png"></a>
+                          </td>
+                          <td style="width:28px;min-width:28px">
+                            <a href="***" style="width:100%; display:block; height:100%"><img style="width:28px;height:50px" src="http://nettel.com.ua/lotzone/tw.png"></a>
+                          </td>
+                          <td style="width:28px;min-width:28px">
+                            <a href="***" style="width:100%; display:block; height:100%"><img style="width:28px;height:50px" src="http://nettel.com.ua/lotzone/gplus.png"></a>
+                          </td>
+                          <td style="width:35%"></td>
+                          <td style="font-family:Arial;font-size:13px">%footer_site% <a href="http://lotzon.com" target="_blank" style="text-decoration: none">lotzon.com</a></td>
+                          <td style="width:20px"></td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <div style="max-width:600px">
+                <table>
+                  <tr>
+                    <td style="width:100%">
+                      <p style="padding-left:20px;padding-right:20px;line-height:13px;font-family: Arial;font-size:11px;color:#9b9b9b;margin-top: 20px">%sub_footer%</p>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
