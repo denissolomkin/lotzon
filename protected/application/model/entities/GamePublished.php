e7d6d26cbc393b938c6dc5b2a1bb93ac5c81b959
denissolomkin <solomkin@gmail.com>
2015-12-24 09:48:46 -0500

    fix loadGame(), key in sql

--- protected/application/model/entities/GamePublished.php
+++ protected/application/model/entities/GamePublished.php
@@ -16,7 +16,8 @@ class GamePublished extends Entity
     }
 
     /* todo delete after merge LOT-22 */
-    public function getOption($key){
+    public function getOption($key)
+    {
         return $this->getOptions($key);
     }
 
@@ -28,7 +29,6 @@ class GamePublished extends Entity
             foreach ($this->getGames() as $gameId) {
 
                 $game = new GameConstructor();
-
                 $game->setId($gameId)
                     /* todo merge tables OnlineGames & QuickGames after merge LOT-22 */
                     ->setType($this->getKey() === 'OnlineGame' ? 'online' : 'chance')
@@ -38,7 +38,7 @@ class GamePublished extends Entity
             }
 
         } catch (EntityException $e) {
-
+            echo $e->getMessage();
         }
 
         return $games;
