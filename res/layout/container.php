63c796aa74ea87f504698ac7ba824449e8db2a84
ubuntu <solomkin@gmail.com>
2016-02-15 16:50:41 +0100

    white banner

--- res/layout/container.php
+++ res/layout/container.php
@@ -24,10 +24,10 @@
 
     <aside class="sidebar">
         <div id="banner-desktop-right" class="banner-4" >
-            <div style="height: 100%; background: #ccc;"></div>
+            <div style="height: 100%;"></div>
         </div>
         <div id="banner-desktop-fixed" class="banner-5">
-            <div style="height: 100%; background: #ccc;"></div>
+            <div style="height: 100%;"></div>
         </div>
     </aside>
 </div><!-- .container -->
