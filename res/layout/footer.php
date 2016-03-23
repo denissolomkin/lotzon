10598333cebc672198d2735d3f462b335db02558
ubuntu <solomkin@gmail.com>
2016-03-23 12:10:32 +0100

    metrica

--- res/layout/footer.php
+++ res/layout/footer.php
@@ -68,11 +68,6 @@
         debugConf = <?php echo json_encode($debug, JSON_PRETTY_PRINT); ?>
 
 </script>
-<!-- Yandex.Metrika informer -->
-<a href="https://metrika.yandex.ru/stat/?id=26806191&amp;from=informer"
-   target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/26806191/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-                                       style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:26806191,lang:'ru'});return false}catch(e){}" /></a>
-<!-- /Yandex.Metrika informer -->
 
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
