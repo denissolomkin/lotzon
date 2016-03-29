a649c1c744a29fd925f025b1130cb301dea05f53
ubuntu <solomkin@gmail.com>
2016-03-29 11:14:25 +0200

    check ya.kit

--- res/js/core/url.js
+++ res/js/core/url.js
@@ -94,7 +94,7 @@
                         Navigation.menu.hide();
                         history.pushState(options.init, "Lotzon", url);
                         Content.updateBanners();
-                        Config.yandexMetrika && window['yaCounter'+Config.yandexMetrika].hit(document.location.protocol + '//' + document.location.host + url, {title: 'Lotzon', referer: oldUrl});
+                        Config.hasOwnProperty('yandexMetrika') && Config.yandexMetrika && window['yaCounter'+Config.yandexMetrika] && window['yaCounter'+Config.yandexMetrika].hit(document.location.protocol + '//' + document.location.host + url, {title: 'Lotzon', referer: oldUrl});
                     }
 
                 }
