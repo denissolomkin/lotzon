427caeaef61aeeff4eb79eb93c9a2450bc7272f2
ubuntu <solomkin@gmail.com>
2016-03-31 15:32:31 +0200

    fix audio

--- res/js/games/Apps.js
+++ res/js/games/Apps.js
@@ -8,13 +8,12 @@
         sample: null,
 
         playAudio: function (key) {
-            console.error('playAudio', key);
             if (!$.cookie("audio-disabled")) {
                 if (typeof key === 'object') {
                     if (Apps.audio && Apps.audio[key[0]] && (file = Apps.audio[key[0]][key[1]]))
-                        $('<audio src=""></audio>').attr('src', 'tpl/audio/' + file).trigger("play");
+                        $('<audio src=""></audio>').attr('src', '/tpl/audio/' + file).trigger("play");
                 } else if (key) {
-                    $('<audio src=""></audio>').attr('src', 'tpl/audio/' + key).trigger("play");
+                    $('<audio src=""></audio>').attr('src', '/tpl/audio/' + key).trigger("play");
                 }
             }
         }
