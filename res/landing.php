3c48218672651449b750285611608083840917a7
subsan <subsan@online.ua>
2016-05-10 13:40:44 +0300

    fix https in og meta

--- res/landing.php
+++ res/landing.php
@@ -25,20 +25,18 @@
     <!-- Schema.org markup for Google+ -->
     <meta itemprop="name" content="<?=$seo['Title'];?>">
     <meta itemprop="description" content="LOTZON. Выигрывает каждый!">
-    <meta itemprop="image" content="http://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
+    <meta itemprop="image" content="https://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
 
     <!-- Twitter Card data -->
-    <meta name="twitter:card" content="http://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
+    <meta name="twitter:card" content="https://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
     <meta name="twitter:title" content="<?=$seo['Title'];?>">
     <meta name="twitter:description" content="LOTZON. Выигрывает каждый!">
     <!-- Twitter summary card with large image must be at least 280x150px -->
-    <meta name="twitter:image:src" content="http://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
+    <meta name="twitter:image:src" content="https://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>">
 
     <!-- Open Graph data -->
     <meta property="og:title" content="<?=$seo['Title'];?>" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="http://www.lotzon.com/" />
-    <meta property="og:image" content="http://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>" />
+    <meta property="og:image" content="https://lotzon.com/res/img/lotzon_soc.png?rnd=<?=rand()?>" />
     <meta property="og:description" content="LOTZON. Выигрывает каждый!" />
     <meta property="article:modified_time" content="<?=date('c', time())?>" />
 
