8f516268ecc3033567da3ffa5c983597a485a5a1
ubuntu <solomkin@gmail.com>
2016-05-25 11:58:43 +0200

    bandit luck

--- protected/application/model/processors/ReportsDBProcessor.php
+++ protected/application/model/processors/ReportsDBProcessor.php
@@ -277,19 +277,17 @@ SELECT CONCAT(YEAR(FROM_UNIXTIME(Date)),' ', MONTHNAME(FROM_UNIXTIME(Date))) `Mo
     {
 
         $sql = "
-        SELECT 
-        CURRENCY,
-        SUM(IF(SUM>0,0,1)) COUNT_PLAY,
-        SUM(IF(SUM>0,1,0)) COUNT_WIN,
+        SELECT CURRENCY, COUNT_PLAY, COUNT_WIN, SUM_PLAY, SUM_WIN, SUM_PLAY - SUM_WIN BANDIT_WIN, CONCAT(ROUND((SUM_PLAY - SUM_WIN)/SUM_PLAY * 100), '%') BANDIT_LUCK FROM
+        (SELECT 
+        CURRENCY, SUM(IF(SUM>0,0,1)) COUNT_PLAY, SUM(IF(SUM>0,1,0)) COUNT_WIN,
         SUM(IF(SUM>0,0,IF(Currency = 'MONEY', Equivalent, Sum)))*-1 SUM_PLAY,
-        SUM(IF(SUM>0,IF(Currency = 'MONEY', Equivalent, Sum),0)) SUM_WIN,
-        SUM(IF(Currency = 'MONEY', Equivalent, Sum))*-1 OUR_PROFIT
+        SUM(IF(SUM>0,IF(Currency = 'MONEY', Equivalent, Sum),0)) SUM_WIN
         FROM `Transactions` 
         WHERE `ObjectType` = 'Slots' 
         AND `Date` > :from
         AND `Date` < :to
         ".(isset($args['Currency']) && $args['Currency']!=''?"AND `Currency` = '{$args['Currency']}'":'')." 
-        GROUP BY CURRENCY";
+        GROUP BY CURRENCY) t";
 
         try {
             $sth = DB::Connect()->prepare($sql);
