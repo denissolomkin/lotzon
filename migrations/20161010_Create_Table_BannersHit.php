311b45b28b72ea287aa89d4f66457390fc5e59c7
subsan <subsan@online.ua>
2016-10-10 18:40:44 +0300

    fix migration on bannershits

--- migrations/20161010_Create_Table_BannersHit.php
+++ migrations/20161010_Create_Table_BannersHit.php
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `BannersHit` (
 `Id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 `Date` int(11) unsigned NOT NULL,
-`UserId` int(10) unsigned NOT NULL,
+`UserId` int(10) unsigned DEFAULT NULL,
 `Device` enum('desktop','tablet','mobile') DEFAULT NULL,
 `Location` enum('brand','top','right','teaser') DEFAULT NULL,
 `Page` enum('default','blog','lottery','games','communication','users','prizes') DEFAULT NULL,
